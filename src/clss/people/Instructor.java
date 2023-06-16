98cfc3cd96f9260fd8a024ae2de12f23102d9558
Cepe_S <santicepeda03@gmail.com>
2023-06-16 15:37:38 -0300

    test commit

--- src/clss/people/Instructor.java
+++ src/clss/people/Instructor.java
@@ -10,6 +10,7 @@ public class Instructor///
 
 
 }
+
 // Pare aqui porque aun no defini bien como va a ser nuestro proyecto si el enfoque es hacia un Gimnasio u
 // un Complejo Deportivo, ya que entre ambos hay diferencia. Ej: en un gimnasio si hay una sala de Maquinas
 // SUM(Por llamarlo de alguna manera) pero no necesariamente Chanchas y Pileta pero en un C.Deportivo Si.
